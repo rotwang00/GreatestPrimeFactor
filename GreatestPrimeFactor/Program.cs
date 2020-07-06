1bd74f42338a5736494ca920e8c8781ff5bdf0c5
Paul Thurmond <paulthurmond@gmail.com>
2020-07-06 08:52:41 -0500

    It actually works now.

--- GreatestPrimeFactor/Program.cs
+++ GreatestPrimeFactor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http.Headers;
 
 namespace GreatestPrimeFactor
 {
@@ -6,7 +7,40 @@ namespace GreatestPrimeFactor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter a positive integer to find the greatest prime factor: ");
+            string stringInput = Console.ReadLine();
+
+            int input = int.Parse(stringInput);
+
+            int working = input;
+
+            int answer = 1;
+
+            // Take care of twos
+            while (working % 2 == 0)
+            {
+                answer = 2;
+                working /= 2;
+            }
+
+            int ceiling = (int)Math.Floor(Math.Sqrt(input));
+
+            // Number is now odd. Just check odd factors.
+            for (int i = 3; i <= ceiling; i = i + 2)
+            {
+                while (working % i == 0)
+                {
+                    answer = i;
+                    working /= i;
+                }
+            }
+
+            if (working == input)
+            {
+                answer = input;
+            }
+
+            Console.WriteLine($"The greatest prime factor of {input} is {answer}");
         }
     }
 }
